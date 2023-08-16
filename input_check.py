f602bb3594290de3a9abde821a3141b89e7c96ce
Rope-IRL <65351495+Rope-IRL@users.noreply.github.com>
2023-08-16 20:04:39 +0300

    Create input_check.py

---
+++ input_check.py
@@ -0,0 +1,23 @@
+import requests
+from bs4 import BeautifulSoup
+
+#Send request to Geonames. If the Country, that user typed, was found on the page, it return city, if it's not, return None
+def inputCheck(city, country):
+    country=country.replace(" ", "+")
+    url=f"https://www.geonames.org/search.html?q={city}+{country}&country="
+    headers={
+        "Accept" : "*/*",
+  #You meed to add your own user-agent
+        "user-agent" : ""
+    }
+
+    req=requests.get(url, headers)
+    src=req.text
+    soup=BeautifulSoup(src,"lxml")
+    
+    checkCity=soup.find("a", string=f"{city}")
+
+    if checkCity !=None:
+        return city
+    else:
+        return None
